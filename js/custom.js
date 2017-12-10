2d3447c13cc034018d093eb9cb05cf49ce07bc56
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2017-12-10 19:11:18 +0100

    Fix js script to hide nav on small screens.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
-//Close Mobile Menu After Click
 $(document).ready(function(){
-    $('.navbar-nav>li>a').on('click', function(){
-        $('.navbar-collapse').collapse('hide');
-    });
+    var $getLink = $('.navbar-nav>li>a')
+    var $getNavCollapse = $('.navbar-collapse');
+
+    function navHide(){
+        $getNavCollapse.collapse('hide');
+    }
+    $getLink.on('click', navHide);
 });
