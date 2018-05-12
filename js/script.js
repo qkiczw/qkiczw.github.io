903f9aa6e6f781d447af30a3255093536328698c
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2018-05-12 15:58:54 +0200

    smooth scroll script added

--- js/script.js
+++ js/script.js
@@ -16,6 +16,23 @@ $(function(){
     $welcomeText.animate({right: '0'}, 600);
     $myLogo.animate({left: '0'}, 700);
 
+    // smooth scroll
+    const $page = $('body, html');
+    const $navbar = $('.navbar-nav');
+    const $navLinks = $navbar.find('a');
 
+    function scrollToSection() {
+        const animationTime = 700;
+        const anchor = $(this).attr('href');
+        $(this)
+            .parent()
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+
+        $page.animate({scrollTop: $(anchor).offset().top}, animationTime);
+    }
+
+$navLinks.on('click', scrollToSection);
 
 });
